da5f0c1acb1568d744336cfab4ad24ef5d605934
jos <wjosdejong@gmail.com>
2014-10-06 14:58:51 +0200

    Printing env variables

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ var GOOGLE_CLIENT_ID = argv.GOOGLE_CLIENT_ID || process.env.GOOGLE_CLIENT_ID;
 var GOOGLE_CLIENT_SECRET = argv.GOOGLE_CLIENT_SECRET || process.env.GOOGLE_CLIENT_SECRET;
 var PORT = argv.PORT || process.env.PORT || 8082;
 
+console.log('process.env', process.env);
+
 if (!GOOGLE_CLIENT_ID) {
   throw new Error('No GOOGLE_CLIENT_ID configured. Provide a GOOGLE_CLIENT_ID via command line arguments or an environment variable');
 }
@@ -43,7 +45,7 @@ console.log('Server listening at http://localhost:' + PORT);
 passport.use(new GoogleStrategy({
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8082/auth/callback",
+      callbackURL: 'http://localhost:8082/auth/callback',
       scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar']
     },
     function(accessToken, refreshToken, profile, done) {
