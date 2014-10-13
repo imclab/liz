544fc002c97e5bf49d6de510a1316d7d671c8cce
jos <wjosdejong@gmail.com>
2014-10-13 16:07:54 +0200

    Implemented intervals.inverse

--- test/intervals.test.js
+++ test/intervals.test.js
@@ -79,7 +79,7 @@ describe('intervals', function() {
 
   describe('merge', function () {
 
-    it('should merge intervals (iso strings)', function () {
+    it('should merge intervals', function () {
       var merged = intervals.merge(busyString);
 
       assert.deepEqual(merged, [
@@ -87,6 +87,78 @@ describe('intervals', function() {
         {start: '2014-10-14T14:00:00Z', end: '2014-10-14T15:30:00Z'},
         {start: '2014-10-15T07:00:00Z', end: '2014-10-15T19:00:00Z'}
       ]);
+    });
+
+  });
+
+  describe('inverse', function () {
+
+    it('should inverse intervals (ISO strings)', function () {
+      var inverse = intervals.inverse([
+        {start: '2014-10-13T07:00:00Z', end: '2014-10-13T14:00:00Z'},
+        {start: '2014-10-13T16:00:00Z', end: '2014-10-13T18:30:00Z'},
+        {start: '2014-10-13T20:00:00Z', end: '2014-10-13T23:00:00Z'}
+      ]);
+
+      assert.deepEqual(inverse, [
+        {start: '2014-10-13T14:00:00Z', end: '2014-10-13T16:00:00Z'},
+        {start: '2014-10-13T18:30:00Z', end: '2014-10-13T20:00:00Z'}
+      ]);
+    });
+
+    it('should inverse intervals (numbers)', function () {
+      var inverse = intervals.inverse([
+        {start: new Date('2014-10-13T07:00:00Z').valueOf(), end: new Date('2014-10-13T14:00:00Z').valueOf()},
+        {start: new Date('2014-10-13T16:00:00Z').valueOf(), end: new Date('2014-10-13T18:30:00Z').valueOf()},
+        {start: new Date('2014-10-13T20:00:00Z').valueOf(), end: new Date('2014-10-13T23:00:00Z').valueOf()}
+      ]);
+
+      assert.deepEqual(inverse, [
+        {start: new Date('2014-10-13T14:00:00Z').valueOf(), end: new Date('2014-10-13T16:00:00Z').valueOf()},
+        {start: new Date('2014-10-13T18:30:00Z').valueOf(), end: new Date('2014-10-13T20:00:00Z').valueOf()}
+      ]);
+    });
+
+    it('should inverse intervals (Date)', function () {
+      var inverse = intervals.inverse([
+        {start: new Date('2014-10-13T07:00:00Z'), end: new Date('2014-10-13T14:00:00Z')},
+        {start: new Date('2014-10-13T16:00:00Z'), end: new Date('2014-10-13T18:30:00Z')},
+        {start: new Date('2014-10-13T20:00:00Z'), end: new Date('2014-10-13T23:00:00Z')}
+      ]);
+
+      assert.deepEqual(inverse, [
+        {start: new Date('2014-10-13T14:00:00Z'), end: new Date('2014-10-13T16:00:00Z')},
+        {start: new Date('2014-10-13T18:30:00Z'), end: new Date('2014-10-13T20:00:00Z')}
+      ]);
+    });
+
+    it('should inverse intervals with a timeMin and timeMax given', function () {
+      var inverse = intervals.inverse([
+        {start: '2014-10-13T07:00:00Z', end: '2014-10-13T14:00:00Z'},
+        {start: '2014-10-13T16:00:00Z', end: '2014-10-13T18:30:00Z'},
+        {start: '2014-10-13T20:00:00Z', end: '2014-10-13T23:00:00Z'}
+      ], '2014-10-13T04:00:00Z', '2014-10-13T23:50:00Z');
+
+      assert.deepEqual(inverse, [
+        {start: '2014-10-13T04:00:00Z', end: '2014-10-13T07:00:00Z'},
+        {start: '2014-10-13T14:00:00Z', end: '2014-10-13T16:00:00Z'},
+        {start: '2014-10-13T18:30:00Z', end: '2014-10-13T20:00:00Z'},
+        {start: '2014-10-13T23:00:00Z', end: '2014-10-13T23:50:00Z'}
+      ]);
+    });
+
+    it('should inverse empty intervals with a timeMin and timeMax given', function () {
+      var inverse = intervals.inverse([], '2014-10-13T07:00:00Z', '2014-10-13T14:00:00Z');
+
+      assert.deepEqual(inverse, [
+        {start: '2014-10-13T07:00:00Z', end: '2014-10-13T14:00:00Z'}
+      ]);
+    });
+
+    it('should inverse empty intervals', function () {
+      var inverse = intervals.inverse([]);
+
+      assert.deepEqual(inverse, []);
     });
 
   });
