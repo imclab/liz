797de71a274318ecfacb291b6e2fc504c2abb41e
jos <wjosdejong@gmail.com>
2014-12-08 09:48:07 +0100

    Neat non-lethal warning message when no email is configured

--- keepalive.js
+++ keepalive.js
@@ -7,15 +7,18 @@ var http = require('http');
 
 function getIndex() {
   var url = 'http://smartplanner.herokuapp.com/index.html';
+  var start = +new Date();
 
   http.get(url, function(res) {
     var body = '';
-    console.log(new Date().toISOString() + ' Status: ' + res.statusCode);
     res.on('data', function(chunk) {
       body += chunk;
     });
     res.on('end', function() {
-      console.log(new Date().toISOString() + ' Bytes: ' + body.length);
+      var end = + new Date();
+      console.log(new Date().toISOString() + ' Status: ' + res.statusCode  +
+        ', Bytes: ' + body.length +
+        ', Latency: ' + (end - start) + ' ms');
     });
   }).on('error', function(err) {
     console.log(new Date().toISOString() + ' Error: ' + err);
